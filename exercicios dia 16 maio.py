b24cb7d0d034843f53a8ad432d2b8bc4cd8629b7
viniooo <130711794+viniooo@users.noreply.github.com>
2023-05-16 20:31:23 -0300

    Add files via upload

---
+++ exercicios dia 16 maio.py
@@ -0,0 +1,15 @@
+#telefone = input("digite seu telefone: ")
+#if telefone[0] == '4' and telefone[1] == '1':
+#    print("é de curitiba")
+#else:
+#    print("não é de curitiba")
+
+salario = []
+
+for cont in range(12):
+    salarios = float(input("digite o salario: "))
+    salario.append(salario)
+
+
+for cont in range(len(salario)):
+    print
